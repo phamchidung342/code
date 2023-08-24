cceec0e4bc78cd4c3b075d06c7c4c81108430c24
phamchidung342 <63303733+phamchidung342@users.noreply.github.com>
2023-08-24 21:12:41 +0700

    Add files via upload

---
+++ homework1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a = 24;
+    int b = 2;
+    printf(" a + b = %d\n", a + b);
+    printf(" a - b = %d\n", a - b);
+    printf(" a * b = %d\n", a * b);
+    printf(" a / b = %d", a / b);
+    return 0;
+}
